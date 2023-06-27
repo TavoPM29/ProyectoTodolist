9b11cad1521d284d490c67965b64ce2541f6dfba
TavoPM29 <gustavomendez291101@gmail.com>
2023-06-27 11:56:57 -0300

    first commit

---
+++ app/lib/app_web/router.ex
@@ -0,0 +1,36 @@
+defmodule AppWeb.Router do
+  use AppWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {AppWeb.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", AppWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+
+    # generic resources for schemas:
+    resources "/items", ItemController
+    resources "/kinds", KindController
+    resources "/lists", ListController
+    resources "/person", PersonController
+    resources "/status", StatusController
+    resources "/timers", TimerController
+
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", AppWeb do
+  #   pipe_through :api
+  # end
+end
